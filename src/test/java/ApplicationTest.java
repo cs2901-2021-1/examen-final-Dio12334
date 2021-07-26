3be3c37f5b894855944f5813addb665d61fd9e62
Dio12334 <diego.paredes.r@utec.edu.pe>
2021-07-26 18:23:47 -0500

    a

--- src/test/java/ApplicationTest.java
+++ src/test/java/ApplicationTest.java
@@ -1,34 +1,47 @@
 import com.diego.Application;
 import com.diego.GestorDeCentros;
-import org.testng.Assert;
+import com.diego.Main;
+import org.junit.Assert;
 import org.testng.annotations.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.logging.Logger;
 
 import static org.testng.AssertJUnit.assertEquals;
 
 public class ApplicationTest {
     @Test
-    void test1(){
+    public void test1() throws FileNotFoundException {
         Application prueba = new Application();
 
-        String input = "add 5";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
+        String[] args = null;
+        final InputStream original = System.in;
+        final FileInputStream fips = new FileInputStream(new File("/home/diego/UTEC/is/examen-final-Dio12334/src/test/java/prueba.txt"));
+        System.setIn(fips);
 
-        assertEquals("5 dda", prueba.reverseString(input));
+        assertEquals("correct", prueba.startMenu(new String[] {"hola", "aloh", "4"}));
+        System.setIn(original);
     }
     @Test
-    void test2(){
+    public void test2(){
         GestorDeCentros prueba = new GestorDeCentros();
         var inicio = System.currentTimeMillis();
         prueba.informacionConsolidada();
         var termino = System.currentTimeMillis();
         assert ((termino - inicio) <= 2000);
+    }
+    @Test
+    public void test3() throws FileNotFoundException {
+
+            String[] args = null;
+            final InputStream original = System.in;
+            final FileInputStream fips = new FileInputStream(new File("/home/diego/UTEC/is/examen-final-Dio12334/src/test/java/prueba.txt"));
+            System.setIn(fips);
+            Main.main(args);
+            System.setIn(original);
+
+
     }
 }
